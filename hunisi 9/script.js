18b2ce1a7095cc224754ed779f05c039b9d8f209
yourusername <your@email.com>
2019-06-09 21:00:34 +0400

    hunisi 9i aracner

---
+++ hunisi 9/script.js
@@ -0,0 +1,24 @@
+var clickCount = 0;
+// function clickHandler(evt){
+//    clickCount++;
+//    console.log(evt);
+//    var str = "Thanks for clicking " + clickCount;
+//    this.innerText = str;
+// }
+
+// var p = document.getElementById("pElement");
+// p.addEventListener("click", clickHandler);
+
+// function bodyClick(evt){
+//     console.log("Clicked at X: " + evt.pageX + ", Y: " + evt.pageY);
+//  }
+//  window.onclick = bodyClick;
+ 
+//  function bodyClick(evt) {
+//     console.log(evt.pageX, evt.pageY);
+//  }
+//  window.onclick = bodyClick;
+ 
+function mouseClicked() {
+    console.log(mouseX, mouseY);
+ }
